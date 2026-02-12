4c7139d2c92cccfa934daa7f67dc9e4e657c7ae3
CICD-Company <jacopo.stucchi1@studenti.unimi.it>
2026-02-12 10:50:27 +0100

    Fix export error

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function greeting(name) {
     return `Hello, ${name}!`;
 }
 
-module.exports = { greeting };
+export default { greeting };
 
 if (require.main === module) {
     console.log(greeting("Baseline User"));
